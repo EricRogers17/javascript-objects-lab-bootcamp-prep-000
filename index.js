d379487ce022d28adaea95d8d899bd4007ae9eb4
Eric Rogers <eric.rog17@gmail.com>
2017-11-11 18:39:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var recipes = new Object();
 
 function updateObjectWithKeyAndValue(object, key, value) {
   // update an existing object without being destructive
-  return Object.assign({}, object, {[key]: value})
+  return Object.assign({}, object, {[key]: value});
+};
 
-}
+
